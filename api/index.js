59a5f6b9dc2747152181c2a8f8de5eceb8a2f7d0
CB <chandrabhushankumar138@gmail.com>
2025-12-07 12:33:41 +0530

    tried

--- api/index.js
+++ api/index.js
@@ -19,6 +19,21 @@ if (process.env.VERCEL) {
 }
 
 module.exports = async (req, res) => {
+    // Enable CORS
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    );
+
+    // Handle OPTIONS request
+    if (req.method === 'OPTIONS') {
+        res.status(200).end();
+        return;
+    }
+
     const { username } = req.query;
 
     if (!username) {
